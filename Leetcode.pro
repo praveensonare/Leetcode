19234be0e35218f698c7634a6ebbc90bbd30fa65
Praveen Sonare <praveens@blackmagicdesign.com>
2020-12-09 12:46:15 +0800

    885. Spiral Matrix III

--- Leetcode.pro
+++ Leetcode.pro
@@ -58,6 +58,7 @@ SOURCES += \
         804_UniqueMorseCodeWords.cpp \
         844_BackspaceStringCompare.cpp \
         845_LongestMountainInArray.cpp \
+        885_SpiralMatrixIII.cpp \
         897_IncreasingOrderSearchTree.cpp \
         902_NumbersAtMostNGivenDigitSet.cpp \
         91_DecodeWays.cpp \
--- main.cpp
+++ main.cpp
@@ -50,6 +50,7 @@ int main()
     test_uniqueMorseRepresentations();  // 804
     test_backspaceCompare();            // 844
     test_longestMountain();             // 845
+    test_spiralMatrixIII();             //885
     test_increasingBST();               // 897
     test_atMostNGivenDigitSet();        // 902
     test_rangeSumBST();                 // 938
